35f923cdd454c3165f78d269e22e1acf3c1ec8fc
ashwath.bharadwaj <ashwath.bharadwaj@73strings.com>
2022-03-27 01:36:44 +0530

    test

--- src/app/showcase/app-routing.module.ts
+++ src/app/showcase/app-routing.module.ts
@@ -10,6 +10,7 @@ import { AppMainComponent } from './app.main.component';
             {path: '', component: AppMainComponent, 
                 children:[
                     {path: '', redirectTo:'setup', pathMatch:'full'},
+                    {path: 'netas', loadChildren: () => import('./components/politicians/avatardemo.module').then(m => m.AvatarDemoModule)},
                     {path: 'setup', loadChildren: () => import('./components/setup/setup.module').then(m => m.SetupModule)},
                     {path: 'theming', loadChildren: () => import('./components/theming/theming.module').then(m => m.ThemingModule)},
                     {path: 'icons', loadChildren: () => import('./components/icons/icons.module').then(m => m.IconsModule)},
