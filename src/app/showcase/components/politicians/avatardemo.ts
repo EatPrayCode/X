3a9b8423962b2849e9aa6cb90fd852c5e22c7943
ashwath.bharadwaj <ashwath.bharadwaj@73strings.com>
2022-03-28 00:25:33 +0530

    test

--- src/app/showcase/components/politicians/avatardemo.ts
+++ src/app/showcase/components/politicians/avatardemo.ts
@@ -4,4 +4,9 @@ import {Component} from '@angular/core';
     templateUrl: './avatardemo.html'
 })
 export class AvatarDemo {
+    activeIndex1: number = 0;
+
+    activeIndex2: number = 0;
+
+    scrollableTabs: any[] = Array.from({ length: 50 }, (_, i) => ({ title: `Tab ${i + 1}`, content: `Tab ${i + 1} Content` }));
 }
