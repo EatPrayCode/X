fec4b867f200505386cb58544f4ab6ff9ec8b92f
ashwath.bharadwaj <ashwath.bharadwaj@73strings.com>
2022-04-05 14:06:58 +0530

    test

--- src/app/showcase/components/politicians/avatardemo.ts
+++ src/app/showcase/components/politicians/avatardemo.ts
@@ -1,12 +1,32 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     templateUrl: './avatardemo.html'
 })
 export class AvatarDemo {
+
+    value = '';
+
     activeIndex1: number = 0;
 
     activeIndex2: number = 0;
 
     scrollableTabs: any[] = Array.from({ length: 50 }, (_, i) => ({ title: `Tab ${i + 1}`, content: `Tab ${i + 1} Content` }));
+
+    OnSearch() {
+        console.log("OnSearch", this.value);
+    }
+
+    OnSearchNext() {
+        console.log("OnSearchNext", this.value);
+    }
+
+    OnSearchPrevious() {
+        console.log("OnSearchPrevious", this.value);
+    }
+
+    OnClear() {
+        console.log("OnClear");
+        this.value = "";
+    }
 }
